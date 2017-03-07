a51259ab1d0eafc39fb851dfd4c1fbde6d3c492d
Andrew <blizzard383@ymail.com>
2017-03-07 14:47:41 -0600

    Update DirectoryEditor.java

--- DirectoryEditor.java
+++ DirectoryEditor.java
@@ -3,20 +3,59 @@ package main;
 import java.util.*;
 
 public class DirectoryEditor {
-static LinkedList<Employee> collection = new LinkedList<Employee>();
+	static LinkedList<Employee> collection = new LinkedList<Employee>();
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		boolean END = false;
+		boolean ENDO = false;
 		Scanner console = new Scanner(System.in);
 		String in;
-		while(!END)
-		{
-			System.out.println("Enter <Your First name>  <Last name>  <Department Name>  <Phone number>");
+		
+		System.out.println("enter a command CLR , ADD, END");
+		while(!ENDO)
+		{	
 			in = console.nextLine();
-			Employee name = new Employee(in);
-			collection.add(name);
-			name.print();
+			if(in.equalsIgnoreCase("ADD"))
+			{
+				boolean ENDI = false;
+				while(ENDI)
+				{
+					System.out.println("Enter <Your First name>  <Last name>  <Department Name>  <Phone number> //( or end to stop)");
+					in = console.nextLine();
+					if(in.equalsIgnoreCase("END"))
+					{
+						ENDI = true;
+						break;
+					}
+					String[] splitted = in.split("\\s+");
+					if(splitted.length == 4)
+					{
+					Employee name = new Employee(in);
+					collection.add(name);
+					}
+					else
+					{
+					
+						System.out.println("invalid format");
+					}
+				}
+			}
+			else if(in.equalsIgnoreCase("CLR"))
+			{
+				
+			}
+			else if(in.equalsIgnoreCase("PRINT"))
+			{
+				//for(Employee i: collection)
+				{
+					
+				}
+			}
+			else if(in.equalsIgnoreCase("END"))
+			{
+				break;
+			}
+			
 		}
 	}
 
